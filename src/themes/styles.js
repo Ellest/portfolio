8d0673f5dbc07a6f0b572a11b8d69721f676ed9b
Ellest <syunelli@gmail.com>
2017-11-02 21:47:31 -0500

    more pages

---
+++ src/themes/styles.js
@@ -0,0 +1,10 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+	@import url('https://fonts.googleapis.com/css?family=Raleway');
+
+	body {
+		font-family: 'Raleway', serif;
+		font-size: 20px;
+	}
+`
